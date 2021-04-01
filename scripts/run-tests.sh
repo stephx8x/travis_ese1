4acd9268a133e2957b66b45d566541a71fa89a95
stephx8x <cdf.lopez.stephane1@gmail.com>
2021-04-01 12:56:46 +0200

    b33

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -1,26 +1,13 @@
 #!/bin/bash
 
-set -ev
-
-name="oui"
+set -e
 
 
-if [ $name == "ouii" ]
-then 
-    echo "ouiiiiiiiiiii"
-else
-    echo "NONNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN"
-fi
-
-
-echo "continuuuuuuuuuuuu"
-
-
-aa=$(ls -l)
-echo "$aa"
 echo "---------------------------------------------"
 
 tox
 mkdir gh-pages
-asciidoctor-pdf text1.adoc -o gh-pages/documentation.pdf
+asciidocto-pdf text1.adoc -o gh-pages/documentation.pdf
 asciidoctor text1.adoc -o gh-pages/index.html
+
+echo "---------------------------------------------"
