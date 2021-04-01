40134cf4c061d1a1119510aad8d193ac608d0d28
stephx8x <cdf.lopez.stephane1@gmail.com>
2021-04-01 08:45:07 +0200

    b2

---
+++ scripts/run-tests.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -ev
+
+name="oui"
+
+
+if [ $name == "ouii" ]
+then 
+    echo "ouiiiiiiiiiii"
+else
+    echo "NONNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN"
+fi
+
+
+echo "continuuuuuuuuuuuu"
+
+
+aa=$(ls -l)
+echo "$aa"
